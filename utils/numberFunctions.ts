5d2f13139de4fdc8d45e9ad12c0cf3505025b3bf
Jake <jacobstefan92@gmail.com>
2021-05-28 10:37:49 +0200

    Add: Func

--- utils/numberFunctions.ts
+++ utils/numberFunctions.ts
@@ -1,13 +1,17 @@
 export function clampN(num: number, min: number, max: number) {
-    const MIN = min || 0;
-    const MAX = max || 20;
-    const parsed = num;
-    return Math.min(Math.max(parsed, MIN), MAX);
+  const MIN = min || 0;
+  const MAX = max || 20;
+  const parsed = num;
+  return Math.min(Math.max(parsed, MIN), MAX);
 }
 
 export function wrap(min: number, max: number, v: number) {
-    if (v > min && v < max) return v;
-    if (v < min) return max;
-    if (v > min && v > max) return min;
-    return min;
+  if (v > min && v < max) return v;
+  if (v < min) return max;
+  if (v > min && v > max) return min;
+  return min;
+}
+
+export function mod(x, m) {
+  return ((x % m) + m) % m;
 }
